13c1dcb7052e5619f84c5e45aec03f9bbce5fe5a
lbxxzmy <creeprw98@outlook.com>
2020-06-07 20:46:41 +0800

    update build

--- src/main/java/com/lance/EnglishApplication.java
+++ src/main/java/com/lance/EnglishApplication.java
@@ -2,6 +2,8 @@ package com.lance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -9,10 +11,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
-public class EnglishApplication {
+public class EnglishApplication extends SpringBootServletInitializer {
 
     public static void main (String[] args) {
         SpringApplication.run(EnglishApplication.class, args);
     }
-
+    //为了打包springboot项目
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
